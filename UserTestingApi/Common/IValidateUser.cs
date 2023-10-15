0a0071adff980fe926dc55cf3c4e412e361e119d
Neg00se <gooosb205@gmail.com>
2023-10-16 02:19:59 +0300

    Add User Validation

---
+++ UserTestingApi/Common/IValidateUser.cs
@@ -0,0 +1,9 @@
+﻿using UserTestingData.Models;
+
+namespace UserTestingApi.Common
+{
+	public interface IValidateUser
+	{
+		bool IsUserValid(User user, string password);
+	}
+}
---
+++ UserTestingApi/Common/ValidateUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserTestingData.Models;
+
+namespace UserTestingApi.Common
+{
+	public class ValidateUser : IValidateUser
+	{
+		private readonly IPasswordHasher<User> _hasher;
+
+		public ValidateUser(IPasswordHasher<User> hasher)
+		{
+			_hasher = hasher;
+		}
+
+		public bool IsUserValid(User user, string password)
+		{
+			var result = _hasher.VerifyHashedPassword(user, user.PasswordHash, password);
+
+			if (result == PasswordVerificationResult.Success)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+}
