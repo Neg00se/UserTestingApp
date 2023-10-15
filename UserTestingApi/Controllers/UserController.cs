3cff2d62a6497da285c6961616a8b82d5976cbb8
Neg00se <gooosb205@gmail.com>
2023-10-16 02:24:07 +0300

    Add User Authentication

--- UserTestingApi/Controllers/UserController.cs
+++ UserTestingApi/Controllers/UserController.cs
@@ -1,35 +1,42 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using UserTestingApi.Repositories;
+using UserTestingApi.Services;
 using UserTestingData.DataAccess;
 using UserTestingData.Models;
 
-namespace UserTestingApi.Controllers
+namespace UserTestingApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class UserController : ControllerBase
+	private readonly IAuthService _authService;
+
+	public UserController(IUserRepository userRepo, IAuthService authService)
 	{
-		private readonly TestingAppDbContext _context;
-		private readonly IPasswordHasher<User> _hasher;
-
-		public UserController(TestingAppDbContext context, IPasswordHasher<User> hasher)
-		{
-			_context = context;
-			_hasher = hasher;
-		}
-
-
-		[HttpPost]
-		public async Task CreateUser(string userName, string password)
-		{
-			User user = new User() { UserName = userName, Role = "User" };
-			user.PasswordHash = _hasher.HashPassword(user, password);
-
-			await _context.Users.AddAsync(user);
-			await _context.SaveChangesAsync();
-
-		}
-
+		_authService = authService;
 	}
+
+
+	[HttpGet]
+	[AllowAnonymous]
+	[Route("Login")]
+	public async Task<IActionResult> Login(string username, string password)
+	{
+		try
+		{
+			var token = await _authService.GenerateToken(username, password);
+			return Ok(token);
+		}
+		catch (Exception ex)
+		{
+
+			return BadRequest(ex.Message);
+		}
+	}
+
+
 }
