96b071e965630330f00394282d1e6cacac51d4c6
Neg00se <gooosb205@gmail.com>
2023-10-16 02:25:14 +0300

    Add Dependency Injection and Jwt Authentication

--- UserTestingApi/Program.cs
+++ UserTestingApi/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using UserTestingApi.Repositories;
 using UserTestingData.DataAccess;
 using UserTestingData.Models;
+using System.Text;
+using UserTestingApi.Common;
+using UserTestingApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,14 +17,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
 
-
-builder.Services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+options.TokenValidationParameters = new TokenValidationParameters
+{
+	ValidateIssuer = true,
+	ValidateAudience = true,
+	ValidateLifetime = true,
+	ValidateIssuerSigningKey = true,
+	ValidIssuer = builder.Configuration["Jwt:Issuer"],
+	ValidAudience = builder.Configuration["Jwt:Audience"],
+	IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+});
 
 builder.Services.AddDbContext<TestingAppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITestRepository, TestRepository>();
+builder.Services.AddScoped<IValidateUser, ValidateUser>();
+builder.Services.AddScoped<IAuthService ,  AuthService>();
+builder.Services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
 
 var app = builder.Build();
 
@@ -31,6 +52,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
