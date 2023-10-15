d1fee67d224dc130b9e56bb2a82e8469a81c694d
Neg00se <gooosb205@gmail.com>
2023-10-16 02:18:42 +0300

    Add Model for user's answers

---
+++ UserTestingApi/Models/IUserAnswersModel.cs
@@ -0,0 +1,10 @@
+﻿using UserTestingData.Models;
+
+namespace UserTestingApi.Models
+{
+	public interface IUserAnswersModel
+	{
+		int QuestionId { get; set; }
+		Answer? UserAnswer { get; set; }
+	}
+}
---
+++ UserTestingApi/Models/UserAnswersModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UserTestingData.Models;
+
+namespace UserTestingApi.Models
+{
+	public class UserAnswersModel : IUserAnswersModel
+	{
+		[Required]
+		public int QuestionId { get; set; }
+		public Answer? UserAnswer { get; set; }
+	}
+}
