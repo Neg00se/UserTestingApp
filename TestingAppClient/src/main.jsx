178cb752054685d2ab69236698f4fc9cdf77bc84
Neg00se <gooosb205@gmail.com>
2023-10-16 21:25:40 +0300

    Add react-router-dom and redux toolkit

--- TestingAppClient/src/main.jsx
+++ TestingAppClient/src/main.jsx
@@ -8,6 +8,10 @@ import { store } from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
